0a56397fcb976c979a16f3c37420979cc847881f
Wook-2 <bwson98@gmail.com>
2020-07-28 15:55:29 +0900

    debug=False

--- server.py
+++ server.py
@@ -169,4 +169,4 @@ def request_entity_too_large(error):
 
 if __name__ == '__main__':
 
-    app.run(debug=True, port=8080, host='0.0.0.0')   
+    app.run(debug=False, port=8080, host='0.0.0.0')   
